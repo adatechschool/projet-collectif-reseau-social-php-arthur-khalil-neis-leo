4394c79dbde81fb4b683d993917ae473b521dd52
KhalilHouni <132214784+KhalilHouni@users.noreply.github.com>
2024-02-06 10:50:00 +0100

    added an "deja un compte ?" option

--- registration.php
+++ registration.php
@@ -97,6 +97,11 @@
                         </dl>
                         <input type='submit'>
                     </form>
+                    <br>
+                     <p>
+                        Déjà un compte ?
+                        <a href='login.php'><strong>Connectez-vous.</strong></a>
+                    </p>
                 </article>
             </main>
         </div>
