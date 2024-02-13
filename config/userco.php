697bd80290977f3a2cd55904bc52e795dfc3bd89
arthur MICHAILESCO <arthur.michailesco@gmail.com>
2024-02-13 14:01:41 +0100

    fix double sessionStart

--- config/userco.php
+++ config/userco.php
@@ -1,6 +1,6 @@
 <?php
 // Inclure le code de connexion à la base de données
-include '../config/config.php';
+include_once '../config/config.php';
 // Connexion à la base de données
 if ($mysqli->connect_errno) {
     echo("Échec de la connexion : " . $mysqli->connect_error);
